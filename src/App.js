a52976c9d0fc173ce13416c44a51a873e2c9098e
Azamat Zorkanov <Azamat_Zorkanov@epam.com>
2018-08-18 16:01:25 +0600

    added redirection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Auth from './modules/auth';
 import Home from './modules/home';
@@ -9,8 +9,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/auth" component={Auth} />
-        <Route path="/home" component={Home} />
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/home" component={Home} />
+          <Redirect to="/home"/>
+        </Switch>
       </div>
     );
   }
